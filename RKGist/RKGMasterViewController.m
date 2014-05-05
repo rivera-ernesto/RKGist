53f5f2058219108a68150516014ece6496500998
利辺羅エルネスト <rivera_ernesto@cyberagent.co.jp>
2014-05-05 13:06:07 +0900

    Replace non-existent @"timeStamp" by @"createdAt"

--- RKGist/RKGMasterViewController.m
+++ RKGist/RKGMasterViewController.m
@@ -63,7 +63,7 @@
     
     // If appropriate, configure the new managed object.
     // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
-    [newManagedObject setValue:[NSDate date] forKey:@"timeStamp"];
+    [newManagedObject setValue:[NSDate date] forKey:@"createdAt"];
     
     // Save the context.
     NSError *error = nil;
